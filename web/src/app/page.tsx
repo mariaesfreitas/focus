9a705c3f60402bf1aa677c710cd48969ed451aea
madusfx <mariaesfreitas@gmail.com>
2024-07-31 14:14:59 -0300

    feat: add about section

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,6 +1,7 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 import logo from '../../public/images/focus-logo.png';
+import lawyer from '../../public/images/lawyer-image.jpg';
 import { Header, SectionDivision } from '@/components';
 
 export default function Home() {
@@ -23,6 +24,24 @@ export default function Home() {
         </button>
       </section>
       <SectionDivision title={'Quem somos'} />
+      <section
+        id="quem-somos"
+        className="flex items-center mt-12 bg-customGold w-full"
+      >
+        <img
+          src={lawyer.src}
+          alt="Aperto de mão entre advogados"
+          className="hidden lg:flex w-1/2 h-auto"
+        />
+        <p className="text-white text-xl font-medium py-12 px-16">
+          Combinamos nossa experiência, especialização e um compromisso sólido
+          com a excelência para fornecer soluções jurídicas sob medida e
+          eficazes. Com anos de atuação no mercado, asseguramos a proteção dos
+          seus negócios e a defesa dos seus direitos com transparência, rapidez
+          e resultados comprovados. Confie em quem compreende e prioriza seus
+          interesses.
+        </p>
+      </section>
     </main>
   );
 }
