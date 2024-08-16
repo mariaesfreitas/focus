656e4d152296b09c18112bd3b8cd3320b18416c6
madusfx <mariaesfreitas@gmail.com>
2024-08-16 09:46:20 -0300

    feat: adding financing page and removing dynamic routes

---
+++ web/src/app/atividades/compra-de-veiculo/page.tsx
@@ -0,0 +1,92 @@
+'use client';
+import { Contact, Footer, WhatsappButton } from '@/components';
+
+const VehiclePurchasePage = () => {
+  return (
+    <main className="flex flex-col items-center">
+      <section className="relative w-full">
+        <div
+          className={`absolute inset-0 bg-cyan-950 brightness-50 bg-cover z-0`}
+        ></div>
+        <div className="flex flex-col items-center relative p-12 md:p-32 z-10">
+          <h1 className="text-white text-3xl font-bold">
+            Adquiriu um veículo com defeito? Saiba como resolver a situação
+          </h1>
+          <p className="mt-4 text-white text-base font-bold text-justify">
+            Comprou um veículo que apresentou problemas? Somos uma consultoria
+            jurídica especializada em recuperar o valor gasto em veículos
+            defeituosos.
+          </p>
+          <span className="mt-16">
+            <WhatsappButton />
+          </span>
+        </div>
+      </section>
+      <section className="flex flex-col items-center p-8">
+        <h1 className="text-3xl text-cyan-950 w-full md:w-3/4">
+          Está enfrentando problemas com o seu veículo recém-adquirido devido a
+          defeitos ou má qualidade?
+        </h1>
+        <div className="mt-8 border-customGold border-2 w-1/2"></div>
+        <p className="mt-12 w-full sm:w-3/4 text-xl text-left sm:text-justify">
+          Comprar um veículo novo ou usado é um momento de grande expectativa.
+          Entretanto, problemas mecânicos, falhas elétricas, defeitos no sistema
+          de freios ou na suspensão são apenas alguns exemplos de situações que
+          podem transformar o sonho em pesadelo.
+          <br /> Ainda, ao procurar resolver a situação com o vendedor, os
+          consumidores apenas encontram descaso e incontáveis desculpas, a fim
+          de não resolver a situação.
+        </p>
+        <div className="mt-8 border-customGold border-2 w-1/2"></div>
+        <h1 className="uppercase font-bold text-slate-900 text-2xl mt-12 text-center">
+          Nossos serviços
+        </h1>
+        <div className="mt-10 w-full sm:w-3/4 flex flex-col sm:flex-row items-center gap-6 sm:gap-12">
+          <p className="w-full sm:w-1/2 text-left sm:text-justify">
+            Temos orgulho de oferecer um serviço especializado e eficaz para
+            consumidores que enfrentam problemas com veículos defeituosos. Com
+            anos de experiência e uma metodologia refinada, estamos prontos para
+            assegurar que seus direitos sejam respeitados e que você obtenha a
+            reparação necessária.
+          </p>
+          <span className="flex justify-center w-full sm:w-1/2">
+            <WhatsappButton />
+          </span>
+        </div>
+        <div className="mt-8 border-customGold border-2 w-1/2"></div>
+        <h1 className="uppercase font-bold text-slate-900 text-2xl mt-12 text-center">
+          Conheça seus direitos
+        </h1>
+        <div className="mt-10 w-full sm:w-3/4 flex flex-col-reverse sm:flex-row items-center gap-6 sm:gap-12">
+          <span className="flex justify-center w-full sm:w-1/2">
+            <WhatsappButton />
+          </span>
+          <div className="w-full sm:w-1/2">
+            <p className="mt-6 text-slate-900 bold text-left sm:text-justify">
+              Identificado o problema no veículo, o consumidor poderá optar, por
+              sua livre escolha, entre as seguintes formas de solucionar o
+              problema:
+            </p>
+            <ol className="mt-6" type="1">
+              <li className="text-slate-900">
+                1. A restituição imediata da quantia paga, com o cancelamento do
+                contrato de financiamento;
+              </li>
+              <li className="text-slate-900">
+                2. A substituição do produto por outro da mesma espécie, em
+                perfeitas condições de uso;
+              </li>
+              <li className="text-slate-900">
+                3. O abatimento proporcional do preço.
+              </li>
+            </ol>
+          </div>
+        </div>
+      </section>
+      <Contact />
+      <Footer />
+    </main>
+  );
+};
+
+export default VehiclePurchasePage;
--- web/src/app/atividades/financiamento/page.tsx
+++ web/src/app/atividades/financiamento/page.tsx
@@ -1,40 +1,7 @@
 'use client';
-import { useEffect, useState } from 'react';
-import { useParams } from 'next/navigation';
 import { Contact, Footer, WhatsappButton } from '@/components';
-import { activitiesJson } from '@/utils/activities';
 
-interface Activity {
-  slug: string;
-  title: string;
-  subtitle: string;
-  title2: string;
-  description: string;
-  bgImage: string;
-}
-
-const ActivitiesPage = () => {
-  const params = useParams();
-  const activity = params.atividade;
-
-  const [data, setData] = useState<Activity | null>(null);
-
-  useEffect(() => {
-    const fetchData = () => {
-      const atividades: Activity[] = activitiesJson;
-      const atividadeData = atividades.find(item => item.slug === activity);
-      setData(atividadeData || null);
-    };
-
-    if (activity) {
-      fetchData();
-    }
-  }, [activity]);
-
-  if (!data) {
-    return <p>Carregando...</p>;
-  }
-
+const FinancingPage = () => {
   return (
     <main className="flex flex-col items-center">
       <section className="relative w-full">
@@ -42,9 +9,13 @@ const ActivitiesPage = () => {
           className={`absolute inset-0 bg-cyan-950 brightness-50 bg-cover z-0`}
         ></div>
         <div className="flex flex-col items-center relative p-12 md:p-32 z-10">
-          <h1 className="text-white text-3xl font-bold">{data.title}</h1>
+          <h1 className="text-white text-3xl font-bold">
+            Está pagando altas taxas no seu financiamento?
+          </h1>
           <p className="mt-4 text-white text-base font-bold text-justify">
-            {data.subtitle}
+            Reduza a taxa de juros de seu contrato e recupere os valores pagos
+            em tarifas de serviços não contratados. Encaminhe agora seu contrato
+            de financiamento para nossa equipe de especialistas analisar.
           </p>
           <span className="mt-16">
             <WhatsappButton />
@@ -53,17 +24,16 @@ const ActivitiesPage = () => {
       </section>
       <section className="flex flex-col items-center p-8">
         <h1 className="text-3xl text-cyan-950 w-full md:w-3/4">
-          {data.title2}
+          Tem dúvidas sobre as taxas de juros aplicadas no seu financiamento?
+          Podemos orientá-lo para garantir condições mais justas.
         </h1>
         <div className="mt-8 border-customGold border-2 w-1/2"></div>
         <p className="mt-12 w-full sm:w-3/4 text-xl text-left sm:text-justify">
-          Comprar um veículo novo ou usado é um momento de grande expectativa.
-          Entretanto, problemas mecânicos, falhas elétricas, defeitos no sistema
-          de freios ou na suspensão são apenas alguns exemplos de situações que
-          podem transformar o sonho em pesadelo.
-          <br /> Ainda, ao procurar resolver a situação com o vendedor, os
-          consumidores apenas encontram descaso e incontáveis desculpas, a fim
-          de não resolver a situação.
+          Embora o Banco Central estabeleça um percentual, os bancos e
+          instituições financeiras continuam a cobrar juros acima do mercado o
+          que acaba impactando diretamente no custo dos empréstimos e
+          financiamentos, tornando o crédito mais caro para os consumidores e
+          dificultando o acesso a condições mais justas.
         </p>
         <div className="mt-8 border-customGold border-2 w-1/2"></div>
         <h1 className="uppercase font-bold text-slate-900 text-2xl mt-12 text-center">
@@ -72,10 +42,10 @@ const ActivitiesPage = () => {
         <div className="mt-10 w-full sm:w-3/4 flex flex-col sm:flex-row items-center gap-6 sm:gap-12">
           <p className="w-full sm:w-1/2 text-left sm:text-justify">
             Temos orgulho de oferecer um serviço especializado e eficaz para
-            consumidores que enfrentam problemas com veículos defeituosos. Com
-            anos de experiência e uma metodologia refinada, estamos prontos para
-            assegurar que seus direitos sejam respeitados e que você obtenha a
-            reparação necessária.
+            consumidores que enfrentam problemas com condições de financiamento
+            inadequadas. Com anos de experiência e uma metodologia refinada,
+            estamos prontos para assegurar que seus direitos sejam respeitados e
+            que você obtenha as condições de crédito justas e necessárias.
           </p>
           <span className="flex justify-center w-full sm:w-1/2">
             <WhatsappButton />
@@ -91,21 +61,22 @@ const ActivitiesPage = () => {
           </span>
           <div className="w-full sm:w-1/2">
             <p className="mt-6 text-slate-900 bold text-left sm:text-justify">
-              Identificado o problema no veículo, o consumidor poderá optar, por
-              sua livre escolha, entre as seguintes formas de solucionar o
-              problema:
+              Identificada a irregularidade no financiamento, o consumidor
+              poderá optar, por sua livre escolha, entre as seguintes formas de
+              solucionar o problema:
             </p>
             <ol className="mt-6" type="1">
               <li className="text-slate-900">
-                1. A restituição imediata da quantia paga, com o cancelamento do
-                contrato de financiamento;
+                1. A restituição imediata dos valores pagos, com o cancelamento
+                do contrato de financiamento;
               </li>
               <li className="text-slate-900">
-                2. A substituição do produto por outro da mesma espécie, em
-                perfeitas condições de uso;
+                2. A renegociação do contrato para adequar as condições de
+                financiamento às taxas de mercado;
               </li>
               <li className="text-slate-900">
-                3. O abatimento proporcional do preço.
+                3. O abatimento proporcional dos encargos financeiros cobrados
+                indevidamente.
               </li>
             </ol>
           </div>
@@ -117,4 +88,4 @@ const ActivitiesPage = () => {
   );
 };
 
-export default ActivitiesPage;
+export default FinancingPage;
