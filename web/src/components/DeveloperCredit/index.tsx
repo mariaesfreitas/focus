0ccbac9e610b4033f5198a096f605f8bcf2b5ddc
madusfx <mariaesfreitas@gmail.com>
2024-09-12 15:05:55 -0300

    feat: update developer credit

--- web/src/components/DeveloperCredit/index.tsx
+++ web/src/components/DeveloperCredit/index.tsx
@@ -1,9 +1,13 @@
 export function DeveloperCredit() {
   return (
     <div className="w-full bg-black p-2">
-      <p className="text-white text-base text-center uppercase">
-        Desenvolvido por Maria Eduarda Freitas
-      </p>
+      <a
+        href="https://www.mariaesfreitas.com.br/"
+        target="blank"
+        className="text-white text-base text-center uppercase"
+      >
+        Desenvolvido por &#123;mariaesfreitas&#125;
+      </a>
     </div>
   );
 }
