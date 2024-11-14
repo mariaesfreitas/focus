910cf4d282eb1ea1fa37c89f1a893c76ddc05e9d
madusfx <mariaesfreitas@gmail.com>
2024-11-14 17:17:46 -0300

    update cors

--- server/app.js
+++ server/app.js
@@ -4,12 +4,22 @@ const express = require('express');
 const routes = require('./src/routes');
 
 const app = express();
-const port = process.env.PORT || 3001
+const port = process.env.PORT || 3001;
+
+const allowedOrigins = ['https://www.focusconjur.com.br', 'https://www.amfadvocacia.adv.br'];
 
 app.use(cors({
-  origin: 'https://www.focusconjur.com.br',
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   optionsSuccessStatus: 200
 }));
+
 app.use(express.json());
 app.use(routes);
 
