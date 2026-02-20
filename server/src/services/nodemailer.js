370700f39d7e8552bcedfc9c87338bc2ef673a0e
Maria Eduarda Freitas <mariaesfreitas@gmail.com>
2026-02-20 15:17:43 -0300

    Update nodemailer.js

--- server/src/services/nodemailer.js
+++ server/src/services/nodemailer.js
@@ -13,7 +13,7 @@ const transporter = nodemailer.createTransport({
 const send = (name, phone, email, message) => {
   transporter.sendMail({
     from: process.env.USER_EMAIL,
-    to: 'adrianmanjinski@gmail.com',
+    to: 'adrian.moraes@moraesfrederico.com.br',
     subject: 'Um novo formulário foi submetido!',
     html: `
     <h1>Você recebeu um novo formulário!</h1>
